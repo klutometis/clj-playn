34779754cc9081db4fdf9d42f333a085c0ad4fbd
Peter Danenberg <pcd@roxygen.org>
2012-03-09 17:19:14 -0800

    Trying, unsuccessfully, to reference resources

--- src/clj_playn.clj
+++ src/clj_playn.clj
@@ -1,19 +1,27 @@
 (ns clj-playn
-  (:import (playn.core Game PlayN)
-           (playn.java JavaPlatform))
+  (:import (playn.core
+            Game
+            Image
+            PlayN
+            )
+           (playn.java
+            JavaPlatform
+            ))
   (:gen-class
    :implements [playn.core.Game]))
 
 (defn -main [& args]
   (let [platform (JavaPlatform/register)]
-    #_(.setPathPrefix (.assets platform) ".")
+    #_(.setPathPrefix (.assets platform) "resources")
     (PlayN/run (clj_playn.))))
 
 (defn -updateRate [this]
   25)
 
 (defn -init [this]
-  )
+  (let [image (.getImage (PlayN/assets) "feynman.png")
+        layer (.createImageLayer (PlayN/graphics))]
+    (.add (.rootLayer (PlayN/graphics)) layer)))
 
 (defn -update [this delta])
 
